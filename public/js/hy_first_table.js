3a53eedc4193e820dc38bd9b5276d7c71aa6f8c5
buddhaMoon <hy51hy@126.com>
2020-10-20 17:54:34 +0800

    same

--- public/js/hy_first_table.js
+++ public/js/hy_first_table.js
@@ -1,5 +1,16 @@
 class HyTableFirst {
     constructor ( root ) {
         this.data = root;
+        this.value = null;
     }
+    setValue (value) {
+        this.value = value;
+        console.log('调用成功',value);
+        return this;
+    };
+    getData () {};
+    upData () {};
+    renderForm () {};
+
+
 };
